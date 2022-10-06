72187c8807b80909413f3ffb30771aa52758c7cc
elinkass <elinkass@tktk.ee>
2022-10-06 18:37:58 +0300

    week4 code

---
+++ Week4Loops/Program.cs
@@ -0,0 +1,14 @@
+﻿
+//For loop
+
+
+
+using System;
+
+for (int i = 1; i < 5; i++) //i++ = i + 1
+{
+    Console.WriteLine("We don't need no education!");
+}
+
+
+Console.WriteLine("Have a nice day!");
